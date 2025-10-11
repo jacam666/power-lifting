c8d90d57c69ecec281b2faaa21e8b396f2d7dcd7
ja cam <ja6cam@gmail.com>
2025-10-11 20:27:27 +0100

    changed bg colours

--- src/app/comp-5/page.tsx
+++ src/app/comp-5/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Comp5Page() {
     return (
-        <div className="flex min-h-screen flex-col items-center  pt-10 px-4">
+        <div className="bg-black flex min-h-screen flex-col items-center  pt-10 px-4">
             <div className="w-full max-w-6xl">
                 <Link href="/">
                     <button className="mb-4 px-4 py-2 bg-blue-600 text-white  rounded hover:bg-blue-700">
@@ -11,7 +11,7 @@ export default function Comp5Page() {
                     </button>
                 </Link>
 
-                <div className="text-center mb-6 my-8">
+                <div className="text-center text-white mb-6 my-8">
                     <h1 className="text-4xl font-bold text-white">Comp Name Here</h1>
                     <p className="text-lg text-white">Date: June 15, 2024</p>
                     <p className="text-lg text-white">Location: ????????</p>
